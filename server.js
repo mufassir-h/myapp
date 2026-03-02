9be6346b4d29ad9dd8664f7bbada5454b1bf2cb2
mufassir-h <work.mufassir@gmail.com>
2026-03-02 15:59:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const version = process.env.VERSION || 'v2.1';
+const version = process.env.VERSION || 'v2.2';
 
 app.get('/', (req, res) => {
-  res.send(`Selamat datang di test server ${version}`);
+  res.send(`Selamat datang di server ${version}`);
 });
 
 app.get('/health', (req, res) => {
